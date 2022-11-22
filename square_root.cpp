efeddb8a0de1110ac92320ad85353e0c70200b91
YOURIxYOURI <bartek98775@gmail.com>
2022-11-22 22:27:31 +0100

    Initial commit

---
+++ square_root.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float sqaure_root(int number, int power, int decimal_point);
+
+int main()
+{
+    cout << sqaure_root(531610, 2, 10);
+}
+
+float sqaure_root(int number, int power, int decimal_point) {
+    float x = 1;
+    float check = 1;
+    float y;
+    float result, before_result;
+    while (pow(x, power) <= number) {
+        result = x;
+        x++;
+    }
+    for (int i = 0; i < decimal_point; i++) {
+        y = pow(10, -i);
+        before_result = result;
+        while (pow(before_result + y * check, power) <= number) {
+            result = before_result + y * check;
+            check++;
+        }
+    }
+    return result;
+}
+
